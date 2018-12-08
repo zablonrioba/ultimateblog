b4f1d17ee2d33ee840e056fac21712801506d192
zablon rioba <riobazablon@gmail.com>
2018-12-08 22:44:12 +0300

    initiliazes project

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+<title>ultimate blog</title>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<body>
+<div class="jumbotron">
+  <h1 class="display-4">ultimate blog</h1>
+  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+  <hr class="my-4">
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+</div>
+
+<div class=" card container">
+    <div class = "row">
+    <div class ="col-md-4">
+    <div class = "card">
+    <div class = "card header">
+            <h3>The title</h3>
+            </div>
+            <div class ="card body">
+            <img src="images/IMG_0008.JPG" class ="img_fluid" alt="image">
+            <p> lorem ipsum sit .......?</p>
+    </div>
+    <div class="card footer">
+    <button class="btn btn-outline-success">Read more</button>
+    </div>
+    </div>
+</div>
+<body>
+    
+</body>
+</html>
